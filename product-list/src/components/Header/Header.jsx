89aaa31157d1e07e76eca393d5fcef1bfa62fee1
ValentinAr7 <valio990210@gmail.com>
2023-07-31 17:28:22 +0300

    [C4] SubHeading and buttons for each section

--- product-list/src/components/Header/Header.jsx
+++ product-list/src/components/Header/Header.jsx
@@ -1,27 +1,31 @@
 import { FaSearch, FaUserCircle } from 'react-icons/fa';
 import styles from './header.module.css';
+import Navigation from '../Subheading/Navigation';
 
 const Header = () => {
   return (
-    <div className={styles.headerSection}>
-      <div className={styles.header}>
-        <div className={styles.logo}>
-          <p>C4 Nexus</p>
-        </div>
+    <div>
+      <div className={styles.headerSection}>
+        <div className={styles.header}>
+          <div className={styles.logo}>
+            <p>C4 Nexus</p>
+          </div>
 
-        <div className={styles.search}>
-          <FaSearch className={styles.searchIcon} />
-          <input
-            className={styles.searchBar}
-            type="text"
-            placeholder="Search for items"
-          />
-        </div>
+          <div className={styles.search}>
+            <FaSearch className={styles.searchIcon} />
+            <input
+              className={styles.searchBar}
+              type="text"
+              placeholder="Search for items"
+            />
+          </div>
 
-        <div className={styles.profile}>
-          <FaUserCircle />
+          <div className={styles.profile}>
+            <FaUserCircle />
+          </div>
         </div>
       </div>
+      <Navigation />
     </div>
   );
 };
--- product-list/src/components/Subheading/Navigation.jsx
+++ product-list/src/components/Subheading/Navigation.jsx
@@ -1,11 +1,20 @@
+import styles from './navigation.module.css';
+import data from '../../mockData.json';
 
 const Navigation = () => {
+  return (
+    <div className={styles.header}>
+      <div className={styles.subHeader}>
+        {data.map((section, index) => {
+          return (
+            <button key={index} className={styles.btn}>
+              {section.category}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            
-        </div>
-    )
-}
-
-export default Navigation
+export default Navigation;
