6953508ea2cdd9e18fa714223888b3d80bdbda45
ValentinAr7 <valio990210@gmail.com>
2023-07-31 23:03:25 +0300

    [C4] Product data for recommended products

--- product-list/src/App.jsx
+++ product-list/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import RecommendedProducts from './components/ProductSections/ProductSections';
 
 function App() {
   return (
     <div>
       <Header />
+      <RecommendedProducts />
     </div>
   );
 }
---
+++ product-list/src/components/ProductSections/ProductSections.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { FaShoppingCart } from 'react-icons/fa';
+import styles from './products.module.css';
+import data from '../../mockData.json';
+
+const ProductCard = ({ product }) => {
+  const stars = Array.from({ length: 5 }, (_, i) =>
+    i < product.rating ? '★' : '☆',
+  );
+
+  return (
+    <div className={styles.card}>
+      <img variant="top" className={styles.cardImg} src={product.imageUrl} />
+      <div className={styles.cardBody}>
+        <h3 className={styles.title}>{product.name}</h3>
+        <p className={styles.description}>{product.shortDescription}</p>
+        <div className={styles.addToCart}>
+          <button className={styles.btn}>
+            Add to bag <FaShoppingCart className={styles.shoppingCart} />{' '}
+          </button>
+          <p className={styles.price}> ${product.price}</p>
+        </div>
+        <div className={styles.stars}>
+          <p className={styles.ratingNumber}>{product.rating}</p>
+          {stars.map((star, i) => (
+            <div>
+              <span className={styles.star} key={i}>
+                {star}
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const RecommendedProducts = () => {
+  const recommendedProducts = data.map(section => section.items[0]);
+
+  return (
+    <div className={styles.recommended}>
+      <h2>Recommended Products</h2>
+      <div className={styles.carouselContainer}>
+        {recommendedProducts.map((product, index) => (
+          <div className={styles.carouselItem} key={index}>
+            <ProductCard product={product} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RecommendedProducts;
